cb71be95d9d0b4f34f5d1578f69d952ec03a0e30
MaireadWCI <mairead.white@students.codeinstitute.net>
2024-08-23 18:54:33 +0000

    Add questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,167 @@
+const oysterQuestions = [
+
+    {
+        count: 1,
+        question: "Which goddess from Greek mythology emerged from the sea on an oyster shell?",
+        answers: [
+            "Aphrodite",
+            "Minerva",
+            "Athena"
+        ],
+        correctAnswer: "Aphrodite"
+    },
+
+    {
+        count: 2,
+        question: "Ostreidae is the scientific name for the edible oyster. Which genus is that of the pearl-forming oysters?",
+        answers: [
+            "Mollusc",
+            "Crassostrea",
+            "Pinctada"
+        ],
+        correctAnswer: "Pinctada"
+    },
+
+    {
+        count: 3,
+        question: "The largest oysters can grow up to three feet long in shell length. Which is the largest pearl-producing oyster?",
+        answers: [
+            "Akoya oyster",
+            "Black-winged pearl oyster",
+            "Silver-lipped oyster"
+        ],
+        correctAnswer: "Silver-lipped oyster"
+    },
+
+    {
+        count: 4,
+        question: "The oyster crab is also known as which?",
+        answers: [
+            "Lobster crab",
+            "Cucumber crab",
+            "Pea crab"
+        ],
+        correctAnswer: "Pea crab"
+    },
+
+    {
+        count: 5,
+        question: "What is an oyster?",
+        answers: [
+            "An insect",
+            "An arachnid",
+            "A mollusc"
+        ],
+        correctAnswer: "A mollusc"
+    },
+
+    {
+        count: 6,
+        question: "Which family do True oysters belong to?",
+        answers: [
+            "Spondylus",
+            "Ostreidae",
+            "Pteriidae"
+        ],
+        correctAnswer: "Ostreidae"
+    },
+
+    {
+        count: 7,
+        question: "How long does it take for oysters to reach their maturity?",
+        answers: [
+            "Two years",
+            "Six months",
+            "One year"
+        ],
+        correctAnswer: "One year"
+    },
+
+    {
+        count: 8,
+        question: "Which is the most widely grown bivalve around the world?",
+        answers: [
+            "The Sydney rock oyster",
+            "The Olympia oyster",
+            "The Pacific oyster"
+        ],
+        correctAnswer: "The Pacific oyster"
+    },
+
+    {
+        count: 9,
+        question: "In which year was the eastern oyster introduced into the waters of California?",
+        answers: [
+            "1929",
+            "1935",
+            "1875"
+        ],
+        correctAnswer: "1875"
+    },
+
+    {
+        count: 10,
+        question: "What elements does the oyster contain?",
+        answers: [
+            "Calcium",
+            "Zinc",
+            "Both are true"
+        ],
+        correctAnswer: "Both are true"
+    },
+
+    {
+        count: 11,
+        question: "What is a group of oyster called?",
+        answers: [
+            "Oyster group",
+            "Oyster reef",
+            "Oyster family"
+        ],
+        correctAnswer: "Oyster reef"
+    },
+
+    {
+        count: 12,
+        question: "How can oysters be eaten?",
+        answers: [
+            "Smoked",
+            "Boiled",
+            "Both are correct"
+        ],
+        correctAnswer: "Both are correct"
+    },
+
+    {
+        count: 13,
+        question: "What is the process of opening the oyster called?",
+        answers: [
+            "Oyster-open",
+            "Oyster-shucking",
+            "Killing oyster"
+        ],
+        correctAnswer: "Oyster-shucking"
+    },
+
+    {
+        count: 14,
+        question: "Which ancient civilization was the first to cultivate oysters?",
+        answers: [
+            "Romans",
+            "Egyptians",
+            "Greeks"
+        ],
+        correctAnswer: "Greeks"
+    },
+
+    {
+        count: 15,
+        question: "During which period did oysters experience a surge in production?",
+        answers: [
+            "18th and 19th centuries",
+            "12th and 13th centuries",
+            "20th and 21st centuries"
+        ],
+        correctAnswer: "18th and 19th centuries"
+    },
+];
